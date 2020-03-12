edad65457e7eb04218cc76a69ed47cdcea2adbf4
wilsonoey60 <61810752+wilsonoey60@users.noreply.github.com>
2020-03-12 15:12:15 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class hello{
+	public static void main (String[] arg)
+	{
+		System.out.println("hallo ini belajar java");
+		System.out.println("nama saya Wilson Jonathan Oey");
+		System.out.println("nim : 32019005");
+		System.out.println("Jurusan D3 Teknik Informatika");
+	}
+}
